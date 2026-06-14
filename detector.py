b97d0024466e5612a6298f193a98e7b367cf6562
Amir Fuad <fuad.amir@strathmore.edu>
2026-06-14 20:31:06 +0300

    Improved DoS detection and filtered victim traffic

--- detector.py
+++ detector.py
@@ -3,6 +3,9 @@ from collections import defaultdict
 import time
 
 INTERFACE = "enp0s3"
+ATTACKER_IP = "192.168.56.3"
+VICTIM_IP = "192.168.56.2"
+
 DOS_THRESHOLD = 50
 TIME_WINDOW = 5
 
@@ -11,6 +14,7 @@ packet_count = defaultdict(list)
 def packet_callback(packet):
     if packet.haslayer(IP):
         src_ip = packet[IP].src
+        dst_ip = packet[IP].dst
         current_time = time.time()
 
         packet_count[src_ip].append(current_time)
@@ -20,11 +24,17 @@ def packet_callback(packet):
             if current_time - t <= TIME_WINDOW
         ]
 
-        print(f"Packet from {src_ip} | Count: {len(packet_count[src_ip])}")
+        count = len(packet_count[src_ip])
 
-        if len(packet_count[src_ip]) > DOS_THRESHOLD:
-            print("\n[ALERT] Possible DoS attack detected!")
-            print(f"Attacker IP: {src_ip}\n")
+        print(f"Packet from {src_ip} to {dst_ip} | Count: {count}")
+
+        if src_ip == ATTACKER_IP and dst_ip == VICTIM_IP and count > DOS_THRESHOLD:
+            print("\n" + "=" * 50)
+            print("[ALERT] DOS ATTACK DETECTED")
+            print(f"ATTACKER IP: {src_ip}")
+            print(f"TARGET IP: {dst_ip}")
+            print(f"PACKETS IN {TIME_WINDOW} SECONDS: {count}")
+            print("=" * 50 + "\n")
 
 print("IPS DoS Detector Started...")
 sniff(iface=INTERFACE, prn=packet_callback, store=False)
